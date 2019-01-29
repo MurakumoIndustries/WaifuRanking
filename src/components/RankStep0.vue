4027c1a04c6e193af8e6afcece4696861a5df7d3
WindWT <leibao0723@gmail.com>
2019-01-29 15:27:58 +0800

    small fix

--- src/components/RankStep0.vue
+++ src/components/RankStep0.vue
@@ -3,6 +3,7 @@
         <h5>Terms Of Service</h5>
         <p>You must read and agree to this Terms of Service Agreement if you are a User of this site.</p>
         <p>You acknowledge and agree that, by clicking the “I Agree” or similar button to access the Services, you have read these terms and agree to these terms. If you do not agree with these terms, you must immediately close this window.</p>
+        <p>You acknowledge and agree that, all rankings generted by the Services are all meaningless.</p>
         <p>You must be rank 100 or greater, and has at least 1 waifu, to be a User of the Services.</p>
 
         <div class="text-right">
